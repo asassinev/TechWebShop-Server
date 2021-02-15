607ba44eff483c609c90303e82c7a8c8fe714150
TravkinEvgeny <asassinev@yandex.ru>
2021-02-15 15:20:12 +0300

    added cors and fix database connect

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 const app            = express();
+var cors = require ('cors');
+app.use(cors({
+    origin:['http://localhost:8080','http://127.0.0.1:8080'],
+    credentials:true
+}));
 const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+MongoClient.connect(db.url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });
+})
