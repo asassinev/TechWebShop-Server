4e907cb9bfa9822ebcdf0f3a0657315f886cb9ff
TravkinEvgeny <asassinev@yandex.ru>
2021-02-25 14:41:32 +0300

    change API

--- app/routes/product_routes.js
+++ app/routes/product_routes.js
@@ -1,8 +1,9 @@
 module.exports = function(app, db) {
   var ObjectID = require('mongodb').ObjectID;
-  const myDB = db.db('TechWebShop')
+  const myDB = db.db('TechWebShop');
   app.get('/productList', (req, res) => {
-    myDB.collection('Product').find(req.query).toArray((err, item) => {
+    console.log(req.query.categories);
+    myDB.collection('product').find({'categories': req.query.categories}).toArray((err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
@@ -10,9 +11,9 @@ module.exports = function(app, db) {
       } 
     });
   });
-  app.get('/productInfo/:id', (req, res) => {
+  app.get('/productInfo/:id/', (req, res) => {
     const details = { '_id':  new ObjectID(req.params.id) };
-    myDB.collection('Product').findOne(details, (err, item) => {
+    myDB.collection('product').findOne(details, (err, item) => {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
