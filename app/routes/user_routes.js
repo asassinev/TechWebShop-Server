2186ce867b510e4796e4627afdc6cac508e4cdd8
TravkinEvgeny <asassinev@yandex.ru>
2021-03-29 23:43:04 +0300

    added check for existing email

--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -5,15 +5,23 @@ module.exports = function(app, db) {
   const myDB = db.db('TechWebShop');
   app.post('/create-user', (req, res) => {
     req.on('data', function (data) {
-      data._id = new ObjectId()
-      myDB.collection('users').insertOne(JSON.parse(data), (err, item) => {
-        if (err) {
-          res.send({'error':'An error has occurred'});
+      myDB.collection('users').findOne({email: JSON.parse(data).email}, (err, item) => {
+        if (item !== null || err) {
+          return res.status(302).send({
+            error: 'Email already exists'
+         });
         } else {
-          res.send({'status':'user added'});
-        } 
+          data._id = new ObjectId()
+          myDB.collection('users').insertOne(JSON.parse(data), (err, item) => {
+            if (err) {
+              res.send({'error':'An error has occurred'});
+            } else {
+              res.send({'status':'user added'});
+            } 
+          })
+        }
       })
-    });
+    })
   })
   app.get('/login-user/:email/:password', (req, res) => {
     myDB.collection('users').findOne({email: req.params.email, password: req.params.password}, (err, item) => {
@@ -21,7 +29,7 @@ module.exports = function(app, db) {
         res.send({'error':'An error has occurred'});
       } else {
         if (item === null) {
-          res.sendStatus(404)
+          res.status(404).send({error: 'Username or password is incorrect'})
         } else {
           res.send(item);
         }
