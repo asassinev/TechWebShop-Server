9ae2b04a1ee5c3211e334600dadf26eedd296869
TravkinEvgeny <asassinev@yandex.ru>
2021-03-31 18:48:38 +0300

    added orders routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,8 @@
 const productRoutes = require('./product_routes');
 const userRoutes = require('./user_routes');
+const orderRoutes = require('./order_routes');
 module.exports = function(app, db) {
   productRoutes(app, db);
   userRoutes(app, db);
+  orderRoutes(app, db);
 };
---
+++ app/routes/order_routes.js
@@ -0,0 +1,31 @@
+const { ObjectId } = require('bson');
+
+module.exports = function(app, db) {
+  var ObjectID = require('mongodb').ObjectID;
+  const myDB = db.db('TechWebShop');
+  app.post('/create-order', (req, res) => {
+    req.on('data', function (data) {
+      data._id = new ObjectId()
+      myDB.collection('orders').insertOne(JSON.parse(data), (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send({'status':'Order added'});
+        } 
+      })
+    })
+  })
+  app.post('/get-orders', (req, res) => {
+    myDB.collection('orders').find({email: req.body.email, phone: req.body.phone}, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        if (item === null) {
+          res.status(404).send({error: 'Nothing'})
+        } else {
+          res.send(item);
+        }
+      }
+    })
+  })
+}
