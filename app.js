14d052ba28b50e4d61624a5be8292812e72e133a
TravkinEvgeny <asassinev@yandex.ru>
2021-04-01 15:13:25 +0300

    test heroku

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ app.use(cors({
     origin:['http://localhost:8080','http://127.0.0.1:8080', "https://asassinev.github.io", "https://tech-web-shop-server.herokuapp.com/"],
     credentials:true
 }));
-const port = 8000;
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+app.set('port', (process.env.PORT || 5000));
 MongoClient.connect(db, {useNewUrlParser: true, useUnifiedTopology: true}, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
-  app.listen(port, () => {
-    console.log('We are live on ' + port);
+  app.listen(app.get('port'), () => {
+    console.log('We are live on ' + app.get('port'));
   });
 })
