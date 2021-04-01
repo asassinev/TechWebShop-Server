67b0d6aab378caafeaaec6562a1f805b4b4db2f9
TravkinEvgeny <asassinev@yandex.ru>
2021-04-01 15:24:37 +0300

    test heroku

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
-const db             = proccess.env.MONGODB_URL
 const app            = express();
 var cors = require ('cors');
 app.use(cors({
-    origin:['http://localhost:8080','http://127.0.0.1:8080', "https://asassinev.github.io", "https://tech-web-shop-server.herokuapp.com/"],
-    credentials:true
+  origin:['http://localhost:8080','http://127.0.0.1:8080', "https://asassinev.github.io", "https://tech-web-shop-server.herokuapp.com/"],
+  credentials:true
 }));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+app.set('MongoDB_URL', (process.env.MONGODB_URL || require('./config/db')))
 app.set('port', (process.env.PORT || 5000));
-MongoClient.connect(db, {useNewUrlParser: true, useUnifiedTopology: true}, (err, database) => {
+MongoClient.connect(app.get('MongoDB_URL'), {useNewUrlParser: true, useUnifiedTopology: true}, (err, database) => {
   if (err) return console.log(err)
   require('./app/routes')(app, database);
   app.listen(app.get('port'), () => {
