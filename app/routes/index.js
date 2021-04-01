dc5d4bdb2e11b589e2901fd923450bbba6ad4887
TravkinEvgeny <asassinev@yandex.ru>
2021-04-01 14:31:00 +0300

    test heroku

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,9 @@ const productRoutes = require('./product_routes');
 const userRoutes = require('./user_routes');
 const orderRoutes = require('./order_routes');
 module.exports = function(app, db) {
+  app.get('/', (req, res) => {
+    res.send('ok')
+  })
   productRoutes(app, db);
   userRoutes(app, db);
   orderRoutes(app, db);
