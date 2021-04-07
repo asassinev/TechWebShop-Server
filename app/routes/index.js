428532eca5d4024b2bf7f75b5a03cce083ad90bd
evgeny travkin <asassinev@yandex.ru>
2021-04-07 14:13:44 +0300

    added method for get catalog

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,6 +5,16 @@ module.exports = function(app, db) {
   app.get('/', (req, res) => {
     res.send('ok')
   })
+  app.post('/catalog', (req, res) => {
+    const myDB = db.db('TechWebShop');
+    myDB.collection('catalog').findOne({categories: req.body.categories}, (err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occurred'})
+      } else {
+        res.send(item)
+      }
+    })
+  })
   productRoutes(app, db);
   userRoutes(app, db);
   orderRoutes(app, db);
